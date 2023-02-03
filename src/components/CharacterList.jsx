86fd1002faf332999ca6c0d4718d53b97ea5a82f
Juan <juandi.21.jdj@gmail.com>
2023-02-03 15:33:31 -0500

    Commit inicial

---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import Characters from "./Characters";
+
+function NavPage(props) {
+  return (
+    <header className="d-flex justify-content-between align-items-center">
+      <p>Page: {props.page}</p>
+      <button
+        className="btn btn-primary btn-sm"
+        onClick={() => props.setPage(props.page + 1)}
+      >
+        page {props.page + 1}
+      </button>
+    </header>
+  );
+}
+
+function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      );
+      const data = await response.json();
+      setloading(false);
+      setCharacters(data.results);
+    }
+
+    fetchData();
+  }, [page]);
+
+  return (
+    <div className="container">
+      <NavPage page={page} setPage={setPage} />
+
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="row">
+          {characters.map((character) => {
+            return (
+              <div className="col-md-4" key={character.id}>
+                <Characters character={character} />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <NavPage page={page} setPage={setPage} />
+    </div>
+  );
+}
+
+export default CharacterList;
